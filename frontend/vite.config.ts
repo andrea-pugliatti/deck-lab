a907d32716d692e767189c4de8eabe17e73840df
andrea-pugliatti <andrea.pugliatti@outlook.it>
2026-06-20 12:36:53 +0200

    add watch and rebuild actions to docker config

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,8 +1,19 @@
 import { defineConfig } from "vite";
 import react, { reactCompilerPreset } from "@vitejs/plugin-react";
 import babel from "@rolldown/plugin-babel";
+import tailwindcss from "@tailwindcss/vite";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), babel({ presets: [reactCompilerPreset()] })],
+  plugins: [tailwindcss(), react(), babel({ presets: [reactCompilerPreset()] })],
+  server: {
+    host: true,
+    port: 5173,
+    proxy: {
+      "/api": {
+        target: process.env.VITE_API_URL || "http://localhost:8080",
+        changeOrigin: true,
+      },
+    },
+  },
 });
