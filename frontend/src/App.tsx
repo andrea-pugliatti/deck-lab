8462b1fba98aca54067ea8a7c2e02203d536a4ee
andrea-pugliatti <andrea.pugliatti@outlook.it>
2026-07-10 12:50:25 +0200

    add @tanstack/react-query for data fetching; wrap app in QueryClientProvider to manage query state

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,3 +1,4 @@
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Route, Routes } from "react-router";
 
 import { AuthProvider } from "./context/AuthContext";
@@ -15,35 +16,46 @@ import Login from "./pages/Login";
 import NotFound from "./pages/NotFound";
 import Register from "./pages/Register";
 
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      refetchOnWindowFocus: false,
+      retry: 1,
+    },
+  },
+});
+
 function App() {
   return (
-    <BrowserRouter>
-      <AuthProvider>
-        <Routes>
-          <Route element={<DefaultLayout />}>
-            <Route index element={<Home />} />
-            <Route path="cards" element={<Cards />} />
-            <Route path="cards/:id" element={<CardDetail />} />
-            <Route path="decks" element={<Decks />} />
-            <Route path="decks/:id" element={<DeckDetail />} />
-            <Route path="simulator" element={<HandSimulator />} />
+    <QueryClientProvider client={queryClient}>
+      <BrowserRouter>
+        <AuthProvider>
+          <Routes>
+            <Route element={<DefaultLayout />}>
+              <Route index element={<Home />} />
+              <Route path="cards" element={<Cards />} />
+              <Route path="cards/:id" element={<CardDetail />} />
+              <Route path="decks" element={<Decks />} />
+              <Route path="decks/:id" element={<DeckDetail />} />
+              <Route path="simulator" element={<HandSimulator />} />
 
-            <Route element={<ProtectedRoute />}>
-              <Route path="my-decks" element={<Decks initialTab="user" />} />
-              <Route path="decks/create" element={<DeckBuilder />} />
-              <Route path="decks/:id/edit" element={<DeckBuilder />} />
+              <Route element={<ProtectedRoute />}>
+                <Route path="my-decks" element={<Decks initialTab="user" />} />
+                <Route path="decks/create" element={<DeckBuilder />} />
+                <Route path="decks/:id/edit" element={<DeckBuilder />} />
+              </Route>
+
+              <Route path="*" element={<NotFound />} />
             </Route>
 
-            <Route path="*" element={<NotFound />} />
-          </Route>
-
-          <Route element={<AuthLayout />}>
-            <Route path="login" element={<Login />} />
-            <Route path="register" element={<Register />} />
-          </Route>
-        </Routes>
-      </AuthProvider>
-    </BrowserRouter>
+            <Route element={<AuthLayout />}>
+              <Route path="login" element={<Login />} />
+              <Route path="register" element={<Register />} />
+            </Route>
+          </Routes>
+        </AuthProvider>
+      </BrowserRouter>
+    </QueryClientProvider>
   );
 }
 
